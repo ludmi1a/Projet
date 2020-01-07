c69665821de82cc710604c044d3b34cf8d805926
LUDMILA <3971280@ppti-14-301-08.ufr-info-p6.jussieu.fr>
2020-01-07 18:12:43 +0100

    pagePerdu

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.static('public'));
 
+let animals = [["chien", "noir", "paris"],["chat", "marron", "lille"]];
+
+for(var i=0; i<animals.length; i++){
+	document.querySelector('.annonce').innerHTML=document.querySelector('.annonce').innerHTML+"<div class=block id="+animals[i][0]+"><ol><li>Race:"+animals[i][0]+"</li><li>Couleur:"+animals[i][1]+"</li><li>Ville:"+animals[i][2]+"</li></ol></div>";
+}
+
 app.engine('hbs', hbs({
   extname: 'hbs',
   defaultLayout: 'index',
